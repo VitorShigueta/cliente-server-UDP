c239a2ae2eb0375d428ec38aa549481e5ff54ba1
Vitor Shigueta <vshigueta@gmail.com>
2021-08-26 19:15:23 -0300

    Primeiro commit

---
+++ clienteUDP.py
@@ -0,0 +1,20 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+print('Cliente Socket criado com sucesso!!!')
+
+host = 'localhost'
+porta = 5433
+message = 'Olá Servidor'
+
+try:
+    print('Cliente: ' + message)
+    s.sendto(message.encode(), (host, 5432))
+
+    dados, servidor = s.recvfrom(4096)
+    dados = dados.decode()
+    print('Cliente: ' + dados)
+finally:
+    print('Cliente: Fechando a Conexão')
+    s.close()
